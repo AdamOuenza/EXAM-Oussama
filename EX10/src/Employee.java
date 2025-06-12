a392f95d40b92c07fb07bf8d3987de1749883ddd
Adam <adamouenza6@gmail.com>
2025-06-12 13:24:45 -0400

    EX10

---
+++ EX10/src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    private String nombre;
+    private String apellido;
+    private double sueldo;
+    private String departamento;
+
+    public Employee(String nombre, String apellido, double sueldo, String departamento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sueldo = sueldo;
+        this.departamento = departamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " | " + departamento + " | " + sueldo;
+    }
+}
