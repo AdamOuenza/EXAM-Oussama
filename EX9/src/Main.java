6197e5da1c2a49d2dd7cdcf9036dd91e9006efda
Adam <adamouenza6@gmail.com>
2025-06-12 13:17:51 -0400

    EX9

---
+++ EX9/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> empleados = List.of(
+                new Employee("alberto", "Garcia", 5000, "IT"),
+                new Employee("Basilio", "Garcia", 7600, "IT"),
+                new Employee("Carlos", "Gomez", 3587.5, "Sales"),
+                new Employee("David", "Gutierrez", 4700.77, "Marketing"),
+                new Employee("Emilio", "Gonzalo", 6200, "IT"),
+                new Employee("Fernando", "Gasmon", 3200, "Sales"),
+                new Employee("Gabriel", "Gyster", 4236.4, "Marketing"),
+                new Employee("Hector", "zabal", 5000, "AI"),
+                new Employee("Ignacio", "xermio", 4300, "AI"),
+                new Employee("Jorge", "yelmes", 5000, "AI"),
+                new Employee("Kevin", "veiles", 2500, "AI"),
+                new Employee("Lucrecio", "tibio", 3210, "IT"),
+                new Employee("Miguel", "ubal", 9000, "ceo"),
+                new Employee("Nicolas", "quilves", 4700, "parking"),
+                new Employee("Omar", "pontrio", 5000, "parking"),
+                new Employee("Pablo", "ovez", 6000, "parking")
+        );
+
+        System.out.println("Empleados ordenados por sueldo (descendente):");
+        empleados.stream()
+                .sorted(Comparator.comparing(Employee::getSueldo).reversed())
+                .forEach(System.out::println);
+
+        System.out.println("\nEmpleados ordenados por departamento y sueldo (ascendente):");
+        empleados.stream()
+                .sorted(Comparator.comparing(Employee::getDepartamento)
+                        .thenComparing(Employee::getSueldo))
+                .forEach(System.out::println);
+    }
+}
